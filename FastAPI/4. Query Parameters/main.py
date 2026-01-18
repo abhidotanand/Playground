1c3e1e7e1ca3c0c2fa6d301d71480ded4419dacf
abhidotanand <abhidotanand+github@gmail.com>
2026-01-18 20:14:12 +0530

    few more things in query parameter

--- FastAPI/4. Query Parameters/main.py
+++ FastAPI/4. Query Parameters/main.py
@@ -9,12 +9,28 @@ products = [
     {"id": 3, "name": "Tablet", "price": 299.99},
 ]
 
+# @app.get("/products")
+# async def get_products(name: Annotated[str | None, Query(max_length=5)] = None
+#                        , max_price: Annotated[float | None, Query(gt=0, lt=10000)] = None):
+#     filtered_products = products
+#     if name:
+#         filtered_products = [product for product in filtered_products if name.lower() in product["name"].lower()]
+#     if max_price is not None:
+#         filtered_products = [product for product in filtered_products if product["price"] <= max_price]
+#     return filtered_products
+
+
 @app.get("/products")
-async def get_products(name: Annotated[str | None, Query(max_length=5)] = None
-                       , max_price: Annotated[float | None, Query(gt=0, lt=10000)] = None):
+async def get_products(search: Annotated[list[str] | None, Query(alias="q", title="Search terms", description="List of search terms")] = None):
     filtered_products = products
-    if name:
-        filtered_products = [product for product in filtered_products if name.lower() in product["name"].lower()]
-    if max_price is not None:
-        filtered_products = [product for product in filtered_products if product["price"] <= max_price]
+    if search:
+        filtered_products = [product for product in filtered_products if any(term.lower() in product["name"].lower() for term in search)]
+    return filtered_products
+
+
+@app.get("/products")
+async def get_products(search: Annotated[list[str] | None, Query(deprecated=True)] = None):
+    filtered_products = products
+    if search:
+        filtered_products = [product for product in filtered_products if any(term.lower() in product["name"].lower() for term in search)]
     return filtered_products
