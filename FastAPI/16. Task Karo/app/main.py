e2c81ec23e9baef3e47f8d22da50f575b3b03af8
abhidotanand <abhidotanand+github@gmail.com>
2026-01-29 13:24:26 +0530

    Added session dependency

--- FastAPI/16. Task Karo/app/main.py
+++ FastAPI/16. Task Karo/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status
-from app.db.config import create_db_and_tables
+from app.db.config import create_db_and_tables, SessionDep
 from contextlib import asynccontextmanager
 from app.tasks.services import *
 from app.tasks.models import *
@@ -15,27 +15,27 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/task", status_code=status.HTTP_201_CREATED, response_model=TaskOut)
-def new_task(new_task: TaskCreate):
+def new_task(new_task: TaskCreate, session: SessionDep):
     task = Task(title=new_task.title, description=new_task.description, completed=new_task.completed)
-    created_task = create_task(task)
+    created_task = create_task(task, session=session)
     return created_task
 
 @app.get("/tasks", status_code=status.HTTP_200_OK, response_model=list[TaskOut])
-def get_all_tasks():
-    tasks = get_tasks()
+def get_all_tasks(session: SessionDep):
+    tasks = get_tasks(session=session)
     return tasks
 
 @app.get("/task/{task_id}", status_code=status.HTTP_200_OK)
-def get_single_task(task_id: int):
-    task = get_task(task_id)
+def get_single_task(task_id: int, session: SessionDep):
+    task = get_task(task_id, session=session)
     return task
 
 @app.put("/task/{task_id}", status_code=status.HTTP_200_OK, response_model=TaskOut)
-def update_existing_task(task_id: int, updated_task: TaskUpdate):
-    updated = update_task(task_id, updated_task)
+def update_existing_task(task_id: int, updated_task: TaskUpdate, session: SessionDep):
+    updated = update_task(task_id, updated_task, session=session)
     return updated
 
 @app.delete("/task/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_existing_task(task_id: int):
-    deleted_task = delete_task(task_id)
+def delete_existing_task(task_id: int, session: SessionDep):
+    deleted_task = delete_task(task_id, session=session)
     return deleted_task
