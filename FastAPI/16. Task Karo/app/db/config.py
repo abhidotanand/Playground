17c3a2a510ad714ccbf490bcbbc7c0d256e190bf
abhidotanand <abhidotanand+github@gmail.com>
2026-01-29 12:56:45 +0530

    Completed taskkaro app

--- FastAPI/16. Task Karo/app/db/config.py
+++ FastAPI/16. Task Karo/app/db/config.py
@@ -1,5 +1,7 @@
 import os
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
+from typing import Annotated
+from fastapi import Depends
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,3 +11,9 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
--- FastAPI/16. Task Karo/app/tasks/services.py
+++ FastAPI/16. Task Karo/app/tasks/services.py
@@ -1,48 +1,43 @@
 from app.db import config
-from app.tasks.models import Task, TaskOut, TaskUpdate, TaskCreate
+from app.tasks.models import Task, TaskOut, TaskUpdate
 from sqlmodel import Session, select
 from fastapi import HTTPException, status
 
-def create_task(task: Task) -> TaskOut:
-    with Session(config.engine) as session:
-        session.add(task)
-        session.commit()
-        session.refresh(task)
-        return task
+def create_task(task: Task, session: Session) -> TaskOut:
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
 
-def get_task(task_id: int) -> TaskOut:
-    with Session(config.engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found.")
-        return task
+def get_task(task_id: int, session: Session) -> TaskOut:
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found.")
+    return task
 
-def get_tasks() -> list[TaskOut]:
-    with Session(config.engine) as session:
-        tasks = session.exec(select(Task)).all()
-        return tasks
+def get_tasks(session: Session) -> list[TaskOut]:
+    tasks = session.exec(select(Task)).all()
+    return tasks
+    
+def update_task(task_id: int, updated_task: TaskUpdate, session: Session) -> TaskOut:
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
+    # task.title = updated_task.title
+    # task.description = updated_task.description
+    # task.completed = updated_task.completed
 
-def update_task(task_id: int, updated_task: TaskUpdate) -> TaskOut:
-    with Session(config.engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
-        # task.title = updated_task.title
-        # task.description = updated_task.description
-        # task.completed = updated_task.completed
+    new_task = updated_task.model_dump()
+    task.sqlmodel_update(new_task)
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
 
-        new_task = updated_task.model_dump()
-        task.sqlmodel_update(new_task)
-        session.add(task)
-        session.commit()
-        session.refresh(task)
-        return task
-
-def delete_task(task_id: int):
-    with Session(config.engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
-        session.delete(task)
-        session.commit()
-        return task
+def delete_task(task_id: int, session: Session):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
+    session.delete(task)
+    session.commit()
+    return task
