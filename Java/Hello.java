06e67fccd6425a19e48455fa8aa7efc4c0d7571c
abhidotanand <abhidotanand+github@gmail.com>
2026-02-09 23:27:46 +0530

    First commitment to learning java

---
+++ Java/Hello.java
@@ -0,0 +1,12 @@
+class Hello {
+
+    public static void main(String a[]) {
+        int num1 = 2;
+        double num2 = 3.2;
+        double res = num1 + num2;
+        boolean boo = true;
+
+        System.out.print(res);
+        System.out.print(boo);
+    }
+}
